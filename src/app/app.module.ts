4c96c0f208c32ea7926a0399d63c2f29397f7b7d
vinay <vinay@indicsoft.com>
2020-03-12 20:21:19 +0530

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+
+
+const routes: Routes = [
+  {path:'about-us', component:AboutusComponent},
+  {path:'contact-us', component:ContactUsComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactUsComponent,
+    SideNavComponent
   ],
   imports: [
+    RouterModule.forRoot( routes, { enableTracing: true }),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule
+    
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
